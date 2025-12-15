187a44455a2de2ad0110914d348678af4e5dcfe7
musaabdullahi2002-svg <musaabdullahi2002-svg>
2025-12-15 20:17:53 +0100

    text updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
-import { Route,Routes } from 'react-router-dom'
+import { Route,Routes } from 'react-router-dom';
 
 function App() {
 
   return (
     <>
+     <header className='header'>
+      <h1>Blogs</h1>
+
+      <nav className='nav-links'>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/categories">Categories</a>
+        <a href="/post">Post</a>
+      </nav>
+     </header>
 
       <Routes>
+      
+
 
       </Routes>
+      
       
     </>
   )
