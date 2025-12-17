eea660fc47de8e4bbdadd1e278b6b9a5af5c8bac
layneeboy555-lgtm <layneeboy555@gmail.com>
2025-12-17 09:45:18 +0100

    update

--- src/Admin/AdminDashboard.jsx
+++ src/Admin/AdminDashboard.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
 
-
+import '../styles/Admin.css'
 export default function AdminDashboard() {
   return (
     <>
       <div className="Dash-board-cont">
+        <h1 className='admin-logo'>Admin Dashboard</h1>
          <div className="child-1">
               <div className="child-2">
-                    
-                  <div className="child-3">
+      
+                <div className="Blog-Management">
+                  <h2>Blog Management</h2>
+                </div>
 
-                  </div>
+                <div className="search-bar">
+                  <input type='text' placeholder=' 🔍︎ Search blog by name....'/>
+                </div>
+
+                <div className="add-blog">
+                  <button className='add'>+ Add New Blog</button>
+                </div>
 
             </div>
          </div>
