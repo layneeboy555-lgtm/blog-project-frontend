908da96650714ea64c5c73d2bf65f0cef462441e
layneeboy555-lgtm <layneeboy555@gmail.com>
2025-12-18 19:52:42 +0100

    text updated

--- src/Admin/AdminDashboard.jsx
+++ src/Admin/AdminDashboard.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
+import { RiDeleteBin5Line } from "react-icons/ri";
+import { FaRegEdit } from "react-icons/fa";
+import { catInformations } from "../Admin/CatinfoDisplay"
 
 import '../styles/Admin.css'
 export default function AdminDashboard() {
   return (
-    <>
+    <>   
       <div className="Dash-board-cont">
+            
         <h1 className='admin-logo'>Admin Dashboard</h1>
-         <div className="child-1">
+        <div className="child-1">
+
               <div className="child-2">
-      
                 <div className="Blog-Management">
                   <h2>Blog Management</h2>
                 </div>
@@ -19,10 +23,27 @@ export default function AdminDashboard() {
 
                 <div className="add-blog">
                   <button className='add'>+ Add New Blog</button>
-                </div>
+              </div>
+              
+          </div>
+            <div className="child-3">
+              
+         
 
-            </div>
-         </div>
+              
+
+            
+                    
+      
+
+          
+              
+             </div>
+
+ 
+            
+                
+        </div>
       </div>
     </>
   )
--- src/Admin/CatinfoDisplay.js
+++ src/Admin/CatinfoDisplay.js
@@ -2,10 +2,70 @@
 
 export const catInformations = [
    {
-     tittle:"",
-     subtittle:"",
-     image:"",
-     category:"",
-     content:"",
+    id:1,
+    tittle:"new laws",
+    subtittle:"laws",
+    category: "politics",
+    date:"",
+    time:12,
+    image:"/tinubu.webp",
    },
+
+   {
+    id:2,
+    tittle:"",
+    subtittle:"",
+    category: "",
+    date:"",
+    time:"",
+    image:"",       
+   },
+
+   {
+    id:3,
+    tittle:"",
+    subtittle:"",
+    category: "",
+    date:"",
+    time:"",
+    image:"",
+  },
+
+  {
+    id:4,
+    tittle:"",
+    subtittle:"",
+    category: "",
+    date:"",
+    time:"",
+    image:"",
+  },
+
+ {
+  id:5,
+    tittle:"",
+    subtittle:"",
+    category: "",
+    date:"",
+    time:"",
+    image:"",
+
+ },
+  
 ]
+
+//  <FaRegEdit /> 
+//  <RiDeleteBin5Line />
+            //  <div className="displayitem">
+            //     {catInformations.map((e)=>(
+                    
+            //         <div className="blogs-cont">
+            //            <img className='image' src={e.image} />
+            //            <h4 className='Display-tittle'>{e.tittle}</h4>
+            //            <h4 className='Display-subtittle'>{e.subtittle}</h4>
+            //            <h4 className='Display-category'>{e.category}</h4>
+            //            <h4 className='Display-time'>{e.time}</h4>
+            //            </div>
+                
+            //     ))} 
+            // </div>
