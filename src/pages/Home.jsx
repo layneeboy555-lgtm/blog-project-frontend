cf153771cc9376e3bca49d55cf6f1af8e4222728
donatusuche222 <donatusuche222@gmail.com>
2025-12-20 17:33:39 +0100

    text upgrade

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,7 +12,8 @@ export default function Home(){
     const handleclick = (data)=>{
         navigate(`/blog/${data.id}`)
     }
-    return(
+    return
+    (
         <>
         <div className="home-container">
             <div className="home">
